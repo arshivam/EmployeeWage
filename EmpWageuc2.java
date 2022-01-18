a14268dfcc53ed1ea84d53e031104cb71dc936e1
arshivam <shivam.armarkar007@gmail.com>
2022-01-18 14:56:08 +0530

    added daily wages of employee

---
+++ EmpWageuc2.java
@@ -0,0 +1,23 @@
+public class EmpWageuc2
+{
+   public static void main(String[] arg)
+   {
+    int isfulltime = 1;
+    int wageperhour = 20;
+    int fulldayhour = 8;
+    double empcheck = Math.floor(Math.random() * 10) % 2;
+    if (empcheck == isfulltime)
+    {
+       int totalsalary = (wageperhour * fulldayhour);
+       System.out.println("Total Salary is" + totalsalary);
+    }
+    else
+    {
+       System.out.println("Salary is 0");
+    }
+
+   }
+
+}
+
+
