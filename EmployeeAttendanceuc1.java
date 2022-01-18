417f88bb2ec60eef64ac929987af5efb2ca5ef16
arshivam <shivam.armarkar007@gmail.com>
2022-01-18 14:36:25 +0530

    whether employee is presnt or absent using random

---
+++ EmployeeAttendanceuc1.java
@@ -0,0 +1,18 @@
+public class EmployeeAttendanceuc1
+{
+   public static void main(String[] arg)
+   {
+    int IS_FULL_TIME = 1;
+    double empcheck = Math.floor(Math.random() * 10) % 2;
+    if (empcheck == IS_FULL_TIME)
+    {
+       System.out.println("employee is present");
+    }
+    else
+    {
+       System.out.println("employee is absent");
+    }
+
+   }
+
+}
