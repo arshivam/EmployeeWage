1560969fa0188d0278d3b4e25193822a0d6954b2
arshivam <96187395+arshivam@users.noreply.github.com>
2022-01-19 15:53:33 +0530

    Update EmpWageuc3.java

--- EmpWageuc3.java
+++ EmpWageuc3.java
@@ -1,28 +1,23 @@
-public class EmpWageuc3
-{
-   public static void main(String[] arg)
-   {
+public class EmpWageuc3 {
+   public static void main(String[] args) {
     int isfulltime = 1;
     int isparttime =2;
     int wageperhour = 20;
     int fulldayhour = 8;
     int parttimehour =4;
     double empcheck = Math.floor(Math.random() * 10) % 3;
-    if (empcheck == isfulltime)
-    {
+    if (empcheck == isfulltime) {
        int totalsalary = (wageperhour * fulldayhour);
        System.out.println("Total Salary is" + " " +totalsalary);
     }
-    else if (empcheck == isparttime)
-    {
+    else if (empcheck == isparttime) {
        int totalsalary = (wageperhour * parttimehour);
        System.out.println("Total Salary is" + " " +totalsalary);
     }
-    else
-    {
+    else {
        System.out.println("Salary is 0");
     }
 
-   }
+  }
 
 }
