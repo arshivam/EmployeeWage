f2d53d741b537de83dfafec38145c359ff42f0c4
arshivam <shivam.armarkar007@gmail.com>
2022-01-19 13:09:24 +0530

    updated programe for working hrs : 100 hr

--- EmpWageuc6.java
+++ EmpWageuc6.java
@@ -1,19 +1,38 @@
 public class EmpWageuc6
 {
-    public static void main(String[] arg)
-  {
-   int wageperhour = 20;
-   int workperday = 8;
-   int workingdays = 1;
-   int salary = 0;
+   public static final int isparttime = 1;
+   public static final int isfulltime = 2;
+   public static final int wageperhour = 20;
+   public static final int Num_Of_Workingdays = 20;
+   public static final int MaxHrsinMonth = 100;
 
-   while (workingdays<=20)
+    public static void main(String[] args)
+  {
+     int totalEmpWage = 0, totalWorkingDays = 0, totalEmpHrs =0;
+
+   while (totalEmpHrs <= MaxHrsinMonth &&
+          totalWorkingDays < Num_Of_Workingdays)
     {
-     int monthlysalary =(wageperhour * workperday * workingdays);
-     salary = monthlysalary;
-     workingdays++;
+      int empHrs = 0;
+      totalWorkingDays++;
+      int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+      switch (empcheck)
+      {
+        case isparttime:
+          empHrs = 4;
+        break;
+        case isfulltime:
+          empHrs = 8;
+        break;
+        default:
+          empHrs = 0;
+        break;
+      }
+      totalEmpHrs += empHrs;
+      int empWage =empHrs * wageperhour;
+      totalEmpWage += empWage;
     }
-      System.out.println("Monthysalary is :"+salary);
 
   }
+
 }
